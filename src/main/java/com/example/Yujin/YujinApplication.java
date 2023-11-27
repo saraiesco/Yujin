a8be2fcbfbc810482714a4eaeff1f9d3ff450583
saraiesco <saraiescobar@google.com>
2023-11-26 21:12:45 -0600

    cors config

--- src/main/java/com/example/Yujin/YujinApplication.java
+++ src/main/java/com/example/Yujin/YujinApplication.java
@@ -2,8 +2,11 @@ package com.example.Yujin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
@@ -17,5 +20,20 @@ public class YujinApplication {
 	@GetMapping("/")
 	public String apiRoot(){
 		return "Yujin";
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry){
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
 	}
 }
